875305f80fff0ccf8ece62dcd7274529c7538f90
Pranav Vinayak Jadhav <pj8841466@gmail.com>
2022-03-07 00:39:12 +0530

    desing part complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { ReactComponent as Divider } from "./images/pattern-divider-desktop.svg";
+import { ReactComponent as Dice } from "./images/icon-dice.svg";
+function click(){
+  console.log(fetch("https://api.adviceslip.com/advice"));
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="Container">
+        <div className="Content flexCon">
+          <p>hii</p>
+          <h1>bye</h1>
+          <div className="dice flexCon">
+            <span className="divider">
+              <Divider />
+            </span>
+            <span className="button flexCon" onClick={click}>
+              <Dice />
+            </span>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
